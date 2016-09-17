d906214e2efc7caf3060d7e6cc374bcb363c2a7f
Nikolay Amiantov <ab@fmap.me>
2016-09-17 23:23:06 +0300

    nwn: init

--- default.nix
+++ default.nix
@@ -1,13 +1,23 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  callPackage = pkgs.newScope self;
+  inherit (pkgs) pkgsi686Linux callPackage_i686;
 
   self = {
-    ut2004Packages = {
-      ut2004-megapack = callPackage ./games/ut2004/megapack.nix { };
+    callPackage = pkgs.newScope self;
 
-      ut2004-gog = callPackage ./games/ut2004/gog.nix { };
+    binkplayer = callPackage_i686 ./applications/video/binkplayer { };
+
+    nwn = self.callPackage ./games/nwn {
+      inherit (pkgsi686Linux.xlibs) libX11 libXcursor;
+      inherit (pkgsi686Linux) libelf SDL SDL_gfx mesa_glu;
+      stdenv = pkgs.stdenv_32bit;
+    };
+
+    ut2004Packages = {
+      ut2004-megapack = self.callPackage ./games/ut2004/megapack.nix { };
+
+      ut2004-gog = self.callPackage ./games/ut2004/gog.nix { };
     };
 
     #ut2004 = pkgs.ut2004Packages.ut2004 (with self.ut2004Packages; [ ut2004-gog ut2004-megapack ]);
