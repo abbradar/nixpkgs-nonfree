6806ddc7c9faeab63f1c0cd661b31dc2d805cb1c
Nikolay Amiantov <ab@fmap.me>
2021-01-09 21:39:57 +0300

    Convert to overlay

--- default.nix
+++ default.nix
@@ -1,54 +1,37 @@
-{ nixpkgs ? import <nixpkgs> { } }:
+self: super: {
+  binkplayer = self.callPackage_i686 ./applications/video/binkplayer { };
 
-let
-  pkgset = pkgs: let
-    inherit (nixpkgs) lib;
+  nwn = self.callPackage ./games/nwn {
+    inherit (self.pkgsi686Linux.xorg) libX11 libXcursor;
+    inherit (self.pkgsi686Linux) libelf SDL SDL_gfx mesa_glu;
+    stdenv = self.stdenv_32bit;
+  };
 
-    core = self: {
-      callPackage = pkgs.newScope self;
+  ut2004Packages = super.ut2004Packages // {
+    ut2004-megapack = self.callPackage ./games/ut2004/megapack.nix { };
 
-      callPackage_i686 = result_i686.callPackage;
+    ut2004-gog = self.callPackage ./games/ut2004/gog.nix { };
+  };
 
-      binkplayer = self.callPackage_i686 ./applications/video/binkplayer { };
+  ut2004-gog = self.ut2004Packages.ut2004 (with self.ut2004Packages; [ ut2004-gog ut2004-megapack ]);
+  ut2004 = self.ut2004Packages.ut2004 (with self.ut2004Packages; [ ut2004-megapack ]);
 
-      nwn = self.callPackage ./games/nwn {
-        inherit (pkgs.pkgsi686Linux.xorg) libX11 libXcursor;
-        inherit (pkgs.pkgsi686Linux) libelf SDL SDL_gfx mesa_glu;
-        stdenv = pkgs.stdenv_32bit;
-      };
+  SDL_1_1 = self.callPackage ./development/libraries/SDL/1.1.nix { };
 
-      ut2004Packages = pkgs.ut2004Packages // {
-        ut2004-megapack = self.callPackage ./games/ut2004/megapack.nix { };
+  utPackages = {
+    ut-goty = self.callPackage ./games/ut/goty.nix { };
 
-        ut2004-gog = self.callPackage ./games/ut2004/gog.nix { };
-      };
+    ut-goty-gog = self.callPackage ./games/ut/gog.nix { };
 
-      ut2004-gog = self.ut2004Packages.ut2004 (with self.ut2004Packages; [ ut2004-gog ut2004-megapack ]);
-      ut2004 = self.ut2004Packages.ut2004 (with self.ut2004Packages; [ ut2004-megapack ]);
+    ut = gamePacks: self.callPackage_i686 ./games/ut/wrapper.nix { inherit gamePacks; };
+  };
 
-      SDL_1_1 = self.callPackage ./development/libraries/SDL/1.1.nix { };
+  ut-gog = self.utPackages.ut (with self.utPackages; [ ut-goty-gog ut-goty ]);
+  ut = self.utPackages.ut (with self.utPackages; [ ut-goty ]);
 
-      utPackages = {
-        ut-goty = self.callPackage ./games/ut/goty.nix { };
+  quake3Packages = {
+    quake3-gog = self.callPackage ./games/quake3/gog.nix { };
+  };
 
-        ut-goty-gog = self.callPackage ./games/ut/gog.nix { };
-
-        ut = gamePacks: self.callPackage_i686 ./games/ut/wrapper.nix { inherit gamePacks; };
-      };
-
-      ut-gog = self.utPackages.ut (with self.utPackages; [ ut-goty-gog ut-goty ]);
-      ut = self.utPackages.ut (with self.utPackages; [ ut-goty ]);
-
-      quake3Packages = {
-        quake3-gog = self.callPackage ./games/quake3/gog.nix { };
-      };
-
-      quake3-gog = pkgs.quake3wrapper { paks = [ self.quake3Packages.quake3-gog pkgs.quake3pointrelease ]; };
-    };
-
-    result = lib.fix core;
-
-    result_i686 = pkgset pkgs.pkgsi686Linux;
-  in result;
-
-in pkgset nixpkgs
+  quake3-gog = self.quake3wrapper { paks = [ self.quake3Packages.quake3-gog self.quake3pointrelease ]; };
+}
